b1cb55334b1fa54940c13ffc9aabc5be9d4187be
Muhammet Ergül <ergules@gmail.com>
2022-02-24 10:13:07 +0300

    add exception handler logs

--- src/main/java/app/vaazar/config/ApiExceptionHandler.java
+++ src/main/java/app/vaazar/config/ApiExceptionHandler.java
@@ -1,5 +1,7 @@
 package app.vaazar.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -11,18 +13,23 @@ import org.springframework.web.context.request.WebRequest;
 @ControllerAdvice
 public class ApiExceptionHandler {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<app.vaazar.config.ErrorDetails> handleAccessDeniedException(AccessDeniedException e, WebRequest req) {
-        return new ResponseEntity<>(new app.vaazar.config.ErrorDetails(e, req), HttpStatus.FORBIDDEN);
+    public ResponseEntity<ErrorDetails> handleAccessDeniedException(AccessDeniedException e, WebRequest req) {
+        log.error(e.getLocalizedMessage());
+        return new ResponseEntity<>(new ErrorDetails(e, req), HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler({IllegalStateException.class, IllegalArgumentException.class, MethodArgumentNotValidException.class})
-    public ResponseEntity<app.vaazar.config.ErrorDetails> handleIllegalStateException(Exception e, WebRequest req) {
-        return new ResponseEntity<>(new app.vaazar.config.ErrorDetails(e, req), HttpStatus.NOT_ACCEPTABLE);
+    public ResponseEntity<ErrorDetails> handleIllegalStateException(Exception e, WebRequest req) {
+        log.error(e.getLocalizedMessage());
+        return new ResponseEntity<>(new ErrorDetails(e, req), HttpStatus.NOT_ACCEPTABLE);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<app.vaazar.config.ErrorDetails> handleUnknownExceptions(Exception e, WebRequest req) {
-        return new ResponseEntity<>(new app.vaazar.config.ErrorDetails(e, req), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<ErrorDetails> handleUnknownExceptions(Exception e, WebRequest req) {
+        log.error("unexpected exception", e);
+        return new ResponseEntity<>(new ErrorDetails(e, req), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
